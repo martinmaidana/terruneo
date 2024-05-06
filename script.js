9eab9826f9902501cb71593159c48a511c3e725e
Martin Maidana <martinnicolasmaidana@gmail.com>
2024-05-06 14:24:22 -0300

    formulario

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function mostrarAlerta() {
     alert("pendiente");
 }
+
+function enviarFormulario() {
+    // Mostrar alerta
+    alert("Email enviado correctamente.");
+    // Redirigir a la página de origen
+    window.location.href = "https://martinmaidana.github.io/terruneo/";
+}
